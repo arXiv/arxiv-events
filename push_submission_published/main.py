05bbefbccfbde5cf162eab1d94e1121601878252
Mark Nazzaro <marknazzaro2@gmail.com>
2023-06-13 15:02:34 -0400

    First draft of publish submission script

---
+++ push_submission_published/main.py
@@ -0,0 +1,52 @@
+import configparser
+import logging
+import json
+
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Row
+from sqlalchemy.exc import OperationalError
+
+from google.cloud.pubsub_v1 import PublisherClient
+
+# config
+config = configparser.ConfigParser()
+config.read("datacite.ini")
+
+# logging
+logger = logging.getLogger("publish_submission_event")
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler(config["DEFAULT"]["log_path"])
+fh.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+# event publisher
+publisher = PublisherClient()
+topic_path = publisher.topic_path(config['DEFAULT']['project_id'], config['DEFAULT']['topic_id'])
+
+# db connection
+try:
+    engine = create_engine(
+            f"mysql+pymysql://{config['DEFAULT']['db_username']}:{config['DEFAULT']['db_password']} \
+            @{config['DEFAULT']['db_host']}/{config['DEFAULT']['db_database_name']}")
+except OperationalError as e:
+    logger.error(f"Failed to connect to DB: {e}")
+    exit()
+
+# get rows
+query = 'select submission_id, document_id, paper_id, version, type \
+         from arXiv_next_mail \
+         where mail_id = (select max(mail_id) \
+                          from arXiv_next_mail)'
+with engine.connect() as conn:
+    rows = conn.execute(query).fetchall()
+
+# send events
+def _format_payload (row: Row) -> str: 
+    return json.dumps({k: v for k, v in row._mapping.items()}).encode('utf-8')
+
+for row in rows:
+    future = publisher.publish(topic_path, _format_payload(row))
+    logger.info(future.result())
